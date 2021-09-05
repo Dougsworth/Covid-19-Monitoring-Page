5eaeac016d62b87f36370cb50ae86f6d7942d839
Jordan-Madden <jordanmadden285@gmail.com>
2021-09-04 23:33:34 -0500

    Integrating SD card

--- src/embedded/embedded.ino
+++ src/embedded/embedded.ino
@@ -19,6 +19,8 @@
 #include <EEPROM.h>
 #include <Wire.h>
 #include <RTC.h>
+#include <SPI.h>
+#include <SD.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
@@ -47,6 +49,10 @@ uint8_t secs;
 #define Echo 8
 #define Trig 9
 Ultrasonic ultrasonic(Trig, Echo);
+
+//SD Card
+#define FILENAME "covid_data.txt"
+File myFile;
 
 //Buzzer
 #define buzzer    A3
@@ -370,6 +376,48 @@ void normalOperation(){
       }      
     }  
   }  
+}
+
+void sdWrite() {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open(FILENAME, FILE_WRITE);
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to" +FILENAME+ "...");
+    myFile.println("This is a test file :)");
+    myFile.println("testing 1, 2, 3.");
+    for (int i = 0; i < 20; i++) {
+      myFile.println(i);
+    }
+    
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void sdRead(){
+  // open the file for reading:
+  myFile = SD.open(FILENAME);
+  if (myFile) {
+    Serial.println(FILENAME);
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } 
+  else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
 
 ISR(PCINT1_vect) {
